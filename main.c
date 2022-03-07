01a7a7e2c3c41371699ca55776d56b4499d34b7e
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-03-07 10:49:28 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int soma = 0, numero;
+
+    while(soma < 1000){
+        printf("Digite o valor para soma: \n");
+        scanf("%d", &numero);
+        //forçar parada com numero negativo
+        if(numero < 0) break;
+        soma += numero;
+    }
+    printf("somatoria: %d \n", soma);
+    return 0;
+}
