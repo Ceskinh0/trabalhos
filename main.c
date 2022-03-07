cd3a4d04e55f6c691f3668d72146a28bf773e09e
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-03-07 09:51:37 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*Implemente um programa que calcule os aumentos de um ordenado
+    para o corrente ano. Se o ordenado for > 1000
+    deve ser aumentado em 5%, se não deve ser aumentado em 7%*/
+
+    double salario;
+
+    printf("Digite o seu salario: \n");
+    scanf("%lf", &salario);
+
+    salario = salario > 1000 ? salario * 1.05 : salario * 1.07;
+
+    printf("seu novo salario eh : RS%.2lf", salario);
+}
