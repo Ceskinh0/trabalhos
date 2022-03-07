ea8f49dc641388e2797daa897b0d25b6dcf33dcd
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-03-07 10:32:46 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ long int i;
+    printf("espere um pouco\n");
+    for(i=0; i<2600000000; i++); //laço sem conteudo
+    printf("a espera acabou\n");
+
+
+
+
+}
