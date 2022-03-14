facb44ebe7f9e573d0cb1d311b5f6d7a46cc23f8
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-03-14 10:43:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num, valor;
+
+    printf("digite o valor a ser calculado: ");
+    scanf("%d", &num);
+
+   for(int i=1; i<=10; i++){
+    valor = num * i;
+
+  printf("\t ||%d X %d = %5d|| \n", num, i, valor);
+    }
+}
