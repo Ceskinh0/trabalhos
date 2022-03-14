0db947bdb12650cfe7e3f4340967f829bfe27c07
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-03-14 10:19:07 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, somat;
+
+    do{
+        do{
+        printf("digite um valor entre 0 e 19:");
+        scanf("%d", &num);
+        }while((num<0) || (num >=20));
+
+    somat += num;
+    }while((num != 0));
+    printf("a somatoria foi: %d \n", somat);
+    return 0;
+}
