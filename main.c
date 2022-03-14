61c4db87fe855b01b9a420fce4e0d112f5375cce
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-03-14 11:06:42 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double valor;
+
+    printf("digite o valor:");
+    scanf("%d", &valor);
+
+    for(int i=1; i<=100; i++){
+    printf("%3d X %3d = %3d | ", valor, i, valor * i);
+        if(i %10 == 0) printf("\n\n");
+    }
+  }
